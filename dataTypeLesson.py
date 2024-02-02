08246fbf23b02de5d657fb49d8fd1a67cb11d903
tadkins12 <158199940+tadkins12@users.noreply.github.com>
2024-02-02 17:50:13 +0000

    data lesson completed

---
+++ dataTypeLesson.py
@@ -0,0 +1,11 @@
+# functions are just instructions that do a job 
+
+# take in user data.
+name= input('what is your name:')
+age= input("what is your age")
+location= input('what is yout location')
+
+# puts out data
+print(name)
+print(age)
+print(location)
