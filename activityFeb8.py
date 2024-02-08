1d91ecabb35db703f9a382564d739326348dc586
tadkins12 <158199940+tadkins12@users.noreply.github.com>
2024-02-08 17:36:05 +0000

    done for the day

---
+++ activityFeb8.py
@@ -0,0 +1,26 @@
+# Using the tools we've learned in class 
+# up to this point create blocks of code that will return a value.
+#(you can use variables, data types, operators, etc.)
+
+# 1. Create a block of code that uses the any of the arithmatic 
+# operator and then print out the result.
+
+number_A = int(input('enter a number'))
+number_B = 20
+print(number_A + number_B)
+
+# 2. Create a block of code that uses the AND operator and and then
+# print out the result.
+
+ismember = True
+goldtier= True
+
+print(ismember and goldtier)
+
+# 3. Create a block of code that uses the NOT EQUAL operator and then 
+# use then print out the result
+
+fruit_A = 'apples'
+fruit_B =  'oranges
+
+print(fruit_A != fruit_B)
---
+++ lesson.py
@@ -0,0 +1,3 @@
+# data casting
+
+# str(.)
