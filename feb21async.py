ae504f405a0c1415007cf27ef0945d5ba7559698
tadkins12 <158199940+tadkins12@users.noreply.github.com>
2024-02-21 17:18:34 +0000

    done with my work

---
+++ feb21async.py
@@ -0,0 +1,12 @@
+# video one: socratic method
+# 1. the socratic method was developed by a greek philosopher, and it is a basically dialogue between people askingand answwring questions.
+# 2. i think it is important to ask questions in software engineering because it coulf get really confuing and knowing everything you can will be crutal to sucsess.
+
+# video two: privacy and data brokering
+# 1. i think a data broker is someone or a company that makes money by collecting peoples personal informaion.
+# 2. i think companies collect and sell peoples data because they want money and to also get data or information to better their company or product.
+
+# video three: al and racial justice
+# 1. i think systemic racism is practices that goes on throughout a socity that result in  and support unfair advantages based on race or ethnicity
+# 2. it is important to build algorithms that exits racial content because its negitive. in my opinion negitivity has no place in this wold. we should continue to support people not bring them down based on their skin color.
+# 3. its important to have people in color involved so that we could have everyones side and not just one select group
