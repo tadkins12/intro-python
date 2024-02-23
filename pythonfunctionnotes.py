985e25b4fd9d9e54d7464228afb635dbc6ce44f2
tadkins12 <158199940+tadkins12@users.noreply.github.com>
2024-02-23 17:53:08 +0000

    doneeeeeeeee

---
+++ pythonfunctionnotes.py
@@ -0,0 +1,31 @@
+# function - a set of instrctions that execute on tast in a program.
+
+# functions have 2 parts:
+# 1. fuction definiton - the instrction on how the fuction works for the program
+
+def goodafternoon():
+    print('good afternoon team!')
+
+# 2. function call - tells the program to run our function instructions
+    
+goodafternoon()
+
+# function parameters and arguments - external data passed into a function. 
+# data type = strings, floats, boolean, etc. data base
+
+def giftcard_add5(user_giftcard):
+    newbalance = user_giftcard  + 5
+    print(f'your new gift card balance is {newbalance} dollars')
+
+giftcard_add5(60)
+
+def giftcard_add10(user_giftcard)
+    newbalance= user_giftcard + 10
+    print(f'your new gift card balance is {newbalance} dollars')
+
+giftcard_add10(60)
+
+def jobsearch_bysalaey(desiredsalary):
+    print('show these results that are in range...')
+    print(f'here are jobs that pay{desiredsalary}')
+     
---
+++ warmup_Feb23.py
@@ -0,0 +1,13 @@
+# question 1:
+# the two parts needed to create a fuction are fuction call and function definition.
+
+# question 2:
+# function parameter is the nameslisted in the fuction definition.
+
+# question 3:  
+
+def giftcard_add5(user_giftcard):
+    newbalance = user_giftcard  + 5
+    print(f'your new gift card balance is {newbalance} dollars')
+
+giftcard_add5(60)
