0a8c3c5d026be0bb797a6b6f9aafecb6963abf69
tadkins12 <158199940+tadkins12@users.noreply.github.com>
2024-03-13 16:04:00 +0000

    done

---
+++ async_mondayMarch11.py
@@ -0,0 +1,45 @@
+# Intro to Python Async Work
+# Monday March 11th, 2024.
+
+# INSTRUCTIONS
+# Create a new python file called async_3_11.py
+# Answer the following questions. Once you've completed the questions, commit
+# and sync your work.
+# This activity must be completed by the end of class in order to recieve
+# a grade.
+
+# REMEMBER - TAKE YOUR TIME AND DO YOUR BEST! DO NOT GIVE UP! 
+
+# 1. Which Python datacasting function would
+# you use if you wanted to convert a string data type
+# into an integer data type? Please write your response
+# in a complete sentence. 
+'i would use the int() function toconvert a string into integer
+'this s a buit in funtion'
+int()
+#keywords
+#FUNCTION
+#CONVERTS A STRING INTO AN INTEGER
+#datacasting - co
+# 2. Create a list called numbCol that contains three (3 ) colors and three (3) numbers.
+numbCol = ['blue','red','green',1,2,3]
+# 3. You have been hired by a University to create
+# a scholarship function. The client would like to provide 
+# students a scholarship to their school based on the following
+# conditions; 
+# - If the user has never gotten a loan before and,
+#-  if the user has never been to college before.
+# The client would like you to use the logical NOT operator that will
+# compare the condtions and return false. If the result is false, the client
+# would also like the program to print the message "congrats! you've gotten the scholarship."
+# the client has given you the choice on how to enter data for your function.
+# you may enter data using input or pass in data into your function as parameters. 
+
+
+
+def check_scholarship_eligability
+    has taken loan = input("have you even gotten a loan before?(yes/no)
+    has_attended_college = input('have you ever been to college?(yes/no)')
+    if not (has_taken_loan and has_attended_college):
+        print('sorry you did not meet scholarship criteria')
+
